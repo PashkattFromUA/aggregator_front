41b5ce699d61bfa068433af2d8381bca6893b852
PashkattFromUA <pashkattfromua@gmail.com>
2024-12-29 14:27:49 +0100

    added fetch

--- src/app/[lang]/menu/page.js
+++ src/app/[lang]/menu/page.js
@@ -7,6 +7,27 @@ export const metadata = {
 };
 
 export default async function MenuPage(props) {
+  const fetchData = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append(
+      "Authorization",
+      "Basic YWRtaW5AYWdncmVnYXRvci5pbzoxMjM0NTY3OA=="
+    );
+
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:8000/api/category/all", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+  };
+
+  fetchData();
+
   const { lang } = await props.params;
   const dictionary = await getDictionary(lang);
   return (

1f196f530662a813e714d9b128cf380a6d9df1f6
PashkattFromUA <pashkattfromua@gmail.com>
2024-12-29 17:16:29 +0100

    working fetching

--- src/app/[lang]/menu/page.js
+++ src/app/[lang]/menu/page.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import LocaleSwitcher from "../../../../components/localeSwitcher";
 import { getDictionary } from "../../../../getDictionaries";
+import { authCredit } from "../../../../credits";
 
 export const metadata = {
   title: "Menu",
@@ -9,29 +10,38 @@ export const metadata = {
 export default async function MenuPage(props) {
   const fetchData = async () => {
     const myHeaders = new Headers();
-    myHeaders.append(
-      "Authorization",
-      "Basic YWRtaW5AYWdncmVnYXRvci5pbzoxMjM0NTY3OA=="
-    );
+    myHeaders.append("Authorization", authCredit);
 
     const requestOptions = {
       method: "GET",
       headers: myHeaders,
-      redirect: "follow",
     };
 
-    fetch("http://localhost:8000/api/category/all", requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.error(error));
+    try {
+      const response = await fetch(
+        "http://localhost:8000/api/category/all",
+        requestOptions
+      );
+      const data = await response.json();
+      console.log(data);
+
+      const catnames = data.map((item) => item.name);
+      console.log(catnames);
+
+      return catnames;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
 
-  fetchData();
+  const catnames = await fetchData();
 
   const { lang } = await props.params;
   const dictionary = await getDictionary(lang);
+
   return (
     <main>
+      {catnames}
       {dictionary.menu}
       <LocaleSwitcher />
     </main>

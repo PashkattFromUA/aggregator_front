20f8dcb2a6ce1dd93f1bda60a65cfe53e1e9108c
PashkattFromUA <pashkattfromua@gmail.com>
2025-01-06 18:15:28 +0100

    reworked footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,89 +1,120 @@
-'use client'
+"use client";
 
-import { useTranslation } from 'react-i18next';
-import Link from 'next/link';
-import styles from '@/styles/footer.module.css'
-import Image from 'next/image';
+import { useTranslation } from "react-i18next";
+import Link from "next/link";
+import styles from "@/styles/footer.module.css";
+import Image from "next/image";
 
 const Footer = (props) => {
+  const { t } = useTranslation();
+  const buttonLabels = props.data;
+  const column1 = buttonLabels.filter((_, index) => index % 2 === 0);
+  const column2 = buttonLabels.filter((_, index) => index % 2 !== 0);
 
-    const { t } = useTranslation();
-    const buttonLabels = props.data;
-    const sortedCategories = buttonLabels.slice().sort((a, b) => a.id - b.id);
-
-    return (
-        <footer className="container">
-            <div className={styles.footertop}>
-                <div className={styles.footerleft}>
-                    <Link href="/">
-                        <Image src='/images/Logo.svg' width={109} height={32} loading='lazy' alt="ABC" />
-                    </Link>
-                    <h2>{t('mainscrtextright')}</h2>
-                </div>
-                <div className={styles.footerright}>
-                    <div className={styles.footer1}>
-                        <h3>{t('navigation')}</h3>
-                        <div className={styles.footerinside}>
-                            <Link href="/aggregator">
-                                <p>{t('aggregator')}</p>
-                            </Link>
-                            <Link href="/faq">
-                                <p>FAQ</p>
-                            </Link>
-                            <Link href="/forpartners">
-                                <p>{t('forpartners')}</p>
-                            </Link>
-                            <Link href="/news">
-                                <p>{t('news')}</p>
-                            </Link>
-                            <Link href="/sitemap">
-                                <p>{t('sitemapbutt')}</p>
-                            </Link>
-                        </div>
-                    </div>
-                    <div className={styles.footer3}>
-                        <h3>{t('follow')}</h3>
-                        <div className={styles.footerinside}>
-                            <Link href="https://t.me/abcrypto_io" target="_blank" rel="noreferrer">
-                                <p>Telegram</p>
-                            </Link>
-                            <Link href="https://twitter.com/abcryptoio" target="_blank" rel="noreferrer">
-                                <p>Twitter</p>
-                            </Link>
-                            <Link href="https://medium.com/@abcrypto_38965" target="_blank" rel="noreferrer">
-                                <p>Medium</p>
-                            </Link>
-                            <Link href="https://www.reddit.com/user/abcrypto_io" target="_blank" rel="noreferrer">
-                                <p>Reddit</p>
-                            </Link>
-                            <Link href="https://www.instagram.com/abcrypto.io/" target="_blank" rel="noreferrer">
-                                <p>Instagram</p>
-                            </Link>
-                        </div>
-                    </div>
-                    <div className={styles.footer2}>
-                        <h3>{t('categories')}</h3>
-                        <div className={styles.foot2but}>
-                            {sortedCategories.map((buttonlabel) => (
-                                <span key={buttonlabel.id}>
-                                    <Link href={`/${buttonlabel.slug}`}>
-                                        {buttonlabel.name}
-                                    </Link>
-                                </span>
-                            ))}
-                        </div>
-                    </div>
-                </div>
+  return (
+    <footer className="container">
+      <div className={styles.footertop}>
+        <div className={styles.footer0}>
+          <Link href="/">
+            <Image
+              src="/images/Logo.svg"
+              width={81}
+              height={32}
+              loading="lazy"
+              alt="ABC"
+            />
+          </Link>
+          <p>{t("mainscrtextright")}</p>
+        </div>
+        <div className={styles.footer1}>
+          <h6>{t("navigation")}</h6>
+          <div className={styles.footerinside}>
+            <Link href="/aggregator">
+              <p>{t("aggregator")}</p>
+            </Link>
+            <Link href="/faq">
+              <p>FAQ</p>
+            </Link>
+            <Link href="/forpartners">
+              <p>{t("forpartners")}</p>
+            </Link>
+            <Link href="/news">
+              <p>{t("news")}</p>
+            </Link>
+            <Link href="/sitemap">
+              <p>{t("sitemapbutt")}</p>
+            </Link>
+          </div>
+        </div>
+        <div className={styles.footer2}>
+          <h6>{t("categories")}</h6>
+          <div className={styles.foot2but}>
+            <div className={styles.column}>
+              {column1.map((buttonlabel) => (
+                <span key={buttonlabel.id}>
+                  <Link href={`/${buttonlabel.slug}`}>{buttonlabel.name}</Link>
+                </span>
+              ))}
             </div>
-            <div className={styles.footerbot}>
-                <h2>{t('copyright')}</h2>
-                <div className={styles.footerbotr}>
-                        <Link href="/privacypolicy">{t('privacy')}</Link>
-                        <Link href="/termsofuse">{t('terms')}</Link>
-                </div>
+            <div className={styles.column}>
+              {column2.map((buttonlabel) => (
+                <span key={buttonlabel.id}>
+                  <Link href={`/${buttonlabel.slug}`}>{buttonlabel.name}</Link>
+                </span>
+              ))}
             </div>
-        </footer>
-    )
-}
+          </div>
+        </div>
+        <div className={styles.footer3}>
+          <h6>{t("follow")}</h6>
+          <div className={styles.footerinside}>
+            <Link
+              href="https://t.me/abcrypto_io"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p>Telegram</p>
+            </Link>
+            <Link
+              href="https://twitter.com/abcryptoio"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p>Twitter</p>
+            </Link>
+            <Link
+              href="https://medium.com/@abcrypto_38965"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p>Medium</p>
+            </Link>
+            <Link
+              href="https://www.reddit.com/user/abcrypto_io"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p>Reddit</p>
+            </Link>
+            <Link
+              href="https://www.instagram.com/abcrypto.io/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p>Instagram</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footerbot}>
+        <p>{t("copyright")}</p>
+        <div className={styles.footerbotr}>
+          <Link href="/privacypolicy">{t("privacy")}</Link>
+          <Link href="/termsofuse">{t("terms")}</Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
